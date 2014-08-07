d03d7dac392fa865fab299f360a5d2bcd97fe656
Stephen Crawley <crawley.stephen@gmail.com>
2014-08-08 01:07:21 +1000

    Tweaks ...

--- Berksfile
+++ Berksfile
@@ -2,5 +2,6 @@
 source "https://api.berkshelf.com"
 #site :opscode
 
+cookbook 'setup', github: 'nectar-cookbooks/setup'
 cookbook 'drupal', path: '.'
 cookbook 'vdd', github: 'drupalprojects/vdd', rel: 'chef/cookbooks/core/vdd', branch: '8.x-1.x'
---
+++ attributes/default.rb
@@ -0,0 +1,17 @@
+node.default["vdd"]["sites"]["drupal7"]["account_name"] = "root"
+node.default["vdd"]["sites"]["drupal7"]["account_pass"] = "root"
+node.default["vdd"]["sites"]["drupal7"]["account_mail"] = "box@example.com"
+node.default["vdd"]["sites"]["drupal7"]["site_name"] = "Drupal 7"
+node.default["vdd"]["sites"]["drupal7"]["site_mail"] = "box@example.com"
+node.default["vdd"]["sites"]["drupal7"]["vhost"]["document_root"] = "drupal7"
+node.default["vdd"]["sites"]["drupal7"]["vhost"]["url"] = "drupal7.dev"
+node.default["vdd"]["sites"]["drupal7"]["vhost"]["alias"] = ["www.drupal7.dev"]
+
+node.default["vdd"]["sites"]["drupal8y"]["account_name"] = "root"
+node.default["vdd"]["sites"]["drupal8"]["account_pass"] = "root"
+node.default["vdd"]["sites"]["drupal8"]["account_mail"] = "box@example.com"
+node.default["vdd"]["sites"]["drupal8"]["site_name"] = "Drupal 8"
+node.default["vdd"]["sites"]["drupal8"]["site_mail"] = "box@example.com"
+node.default["vdd"]["sites"]["drupal8"]["vhost"]["document_root"] = "drupal8"
+node.default["vdd"]["sites"]["drupal8"]["vhost"]["url"] = "drupal8.dev"
+node.default["vdd"]["sites"]["drupal8"]["vhost"]["alias"] = ["www.drupal8.dev"]
