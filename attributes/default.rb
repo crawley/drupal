6a2716b264c36de16cf20fbd2447c6768965a709
Stephen Crawley <crawley.stephen@gmail.com>
2014-08-10 21:44:08 +1000

    The 'sites' attributes should really be hash of hashes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,20 +7,31 @@ node.default["vm"]["synced_folders"] = [
                                         } ] 
 node.default["vm"]["forwarded_ports"] = []
 
-node.default["vdd"]["sites"]["drupal7"]["account_name"] = "root"
-node.default["vdd"]["sites"]["drupal7"]["account_pass"] = "root"
-node.default["vdd"]["sites"]["drupal7"]["account_mail"] = "box@example.com"
-node.default["vdd"]["sites"]["drupal7"]["site_name"] = "Drupal 7"
-node.default["vdd"]["sites"]["drupal7"]["site_mail"] = "box@example.com"
-node.default["vdd"]["sites"]["drupal7"]["vhost"]["document_root"] = "drupal7"
-node.default["vdd"]["sites"]["drupal7"]["vhost"]["url"] = "drupal7.dev"
-node.default["vdd"]["sites"]["drupal7"]["vhost"]["alias"] = ["www.drupal7.dev"]
+node.default["drupal"]["sites"] = {}
 
-node.default["vdd"]["sites"]["drupal8y"]["account_name"] = "root"
-node.default["vdd"]["sites"]["drupal8"]["account_pass"] = "root"
-node.default["vdd"]["sites"]["drupal8"]["account_mail"] = "box@example.com"
-node.default["vdd"]["sites"]["drupal8"]["site_name"] = "Drupal 8"
-node.default["vdd"]["sites"]["drupal8"]["site_mail"] = "box@example.com"
-node.default["vdd"]["sites"]["drupal8"]["vhost"]["document_root"] = "drupal8"
-node.default["vdd"]["sites"]["drupal8"]["vhost"]["url"] = "drupal8.dev"
-node.default["vdd"]["sites"]["drupal8"]["vhost"]["alias"] = ["www.drupal8.dev"]
+node.default["vdd"]["sites"] = {
+  "drupal7" : { 
+    "account_name" => "root",
+    "account_pass" => "root",
+    "account_mail" => "box@example.com",
+    "site_name" => "Drupal 7",
+    "site_mail" => "box@example.com",
+    "vhost" => {
+      "document_root" => "drupal7",
+      "url" => "drupal7.dev",
+      "alias" => "www.drupal7.dev"
+    }
+  },
+  "drupal8" : { 
+    "account_name" => "root",
+    "account_pass" => "root",
+    "account_mail" => "box@example.com",
+    "site_name" => "Drupal 8",
+    "site_mail" => "box@example.com",
+    "vhost" => {
+      "document_root" => "drupal8",
+      "url" => "drupal8.dev",
+      "alias" => "www.drupal8.dev"
+    }
+  }
+}
